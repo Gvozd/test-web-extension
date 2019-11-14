f41b36e27a071be1643ab0a62e339578b3415bba
Gvozd <gvozdev.viktor@gmail.com>
2019-11-14 23:30:26 +0300

    базовое получение закладок из конкретной папки, и обработка их мета-данных

--- source/Options/index.tsx
+++ source/Options/index.tsx
@@ -1,6 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {browser, Bookmarks} from 'webextension-polyfill-ts';
+import {safeLoad} from 'js-yaml';
 
 import Options from './Options';
 
 ReactDOM.render(<Options />, document.getElementById('options-root'));
+
+const rootId = '4921';
+
+(async function main(): Promise<void> {
+    const tree: Bookmarks.BookmarkTreeNode[] = await browser.bookmarks.getTree();
+
+    console.time('get bookmarks');
+    const filter = (subtree: Bookmarks.BookmarkTreeNode): boolean => getMetaData(subtree).tags.length > 0;
+    for (const subTree of iterate(tree, {filter})) {
+        console.log(subTree, getMetaData(subTree).tags);
+    }
+    console.timeEnd('get bookmarks');
+})();
+
+function* iterate(
+    tree: Bookmarks.BookmarkTreeNode[],
+    {
+        filter = (): boolean => {return true},
+        needYield = false
+    }: {
+        filter?: BookmarkTreeNodeFilter,
+        needYield?: boolean
+    } = {}
+): Generator<Bookmarks.BookmarkTreeNode> {
+    for (const subTree of tree) {
+        const {id, url, children} = subTree;
+        if (needYield && url && filter(subTree)) {
+            yield subTree;
+        }
+        if (children) {
+            yield* iterate(children, {
+                filter,
+                needYield: needYield || id === rootId
+            });
+        }
+    }
+}
+type BookmarkTreeNodeFilter = (node: Bookmarks.BookmarkTreeNode) => boolean;
+type BookmarkMeta = { tags: string[] };
+function getMetaData({title}: Bookmarks.BookmarkTreeNode): BookmarkMeta {
+    const separatedTitle = title.split('@@');
+    for (let start = separatedTitle.length - 1; start >= 0; start -= 1) {
+        for (let end = separatedTitle.length; end >= start; end -= 1) {
+            try {
+                const json = safeLoad(separatedTitle.slice(start, end).join('@@')) as BookmarkMeta;
+                if (typeof json === 'object' && json.tags) {
+                    return json;
+                }
+            } catch(e) {
+                // skip
+            }
+        }
+    }
+    return {tags:[]};
+}
