a862379e45ba34ba44e098472a7095489991bb2b
Gvozd <gvozdev.viktor@gmail.com>
2019-11-23 01:00:07 +0300

    Базовое отображение тегов в списке TODO - обновлять отображение в списке, после выхода из диалога

--- source/Options/BookmarksList/index.tsx
+++ source/Options/BookmarksList/index.tsx
@@ -2,26 +2,33 @@ import React, {Component, ReactElement} from 'react';
 import {List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import {Folder} from '@material-ui/icons';
 import {match} from 'JSONSelect';
+import Tag from 'antd/lib/tag';
+import 'antd/lib/tag/style/index.css';
+import {observer} from 'mobx-react';
 import {BookmarkTreeNode} from '../model';
 import BookmarkOptions from '../BookmarkOptions';
 
+@observer
 export default class BookmarksList extends Component<{className: string, root: BookmarkTreeNode, treeId: string}>{
     render(): ReactElement {
         const {className, treeId, root} = this.props;
-        const children = match(`object:has(:root>.id:val("${treeId}")) > .children`, root)[0] as BookmarkTreeNode[];
+        const children: BookmarkTreeNode[] = match(`object:has(:root>.id:val("${treeId}")) > .children`, root)[0] as BookmarkTreeNode[];
         return (
             <main className={className}>
                 <List>
                     {
                         children.map((node) => {
-                            const {id, title, children} = node;
+                            const {id, title, children: hasChildren, meta: {tags}} = node;
                             return (
                                 <ListItem key={id}>
-                                    {children ? <ListItemIcon><Folder/></ListItemIcon> : null}
+                                    {hasChildren ? <ListItemIcon><Folder/></ListItemIcon> : null}
                                     <ListItemText primary={
                                         <>
                                             {title}
                                             <BookmarkOptions root={node} />
+                                            {tags.map((tag) => {
+                                                return <Tag>{tag}</Tag>;
+                                            })}
                                         </>
                                     } />
                                 </ListItem>
